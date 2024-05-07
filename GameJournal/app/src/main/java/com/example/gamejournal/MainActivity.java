c0e447fb7a1c2dc7ec5a2b9b5af333df17c98723
RaulBreaFernandez <145338589+RaulBreaFernandez@users.noreply.github.com>
2024-05-07 20:40:36 +0200

    Añadido slider horizontal para carátulas de juegos en MAIN

--- GameJournal/app/src/main/java/com/example/gamejournal/MainActivity.java
+++ GameJournal/app/src/main/java/com/example/gamejournal/MainActivity.java
@@ -1,14 +1,92 @@
 package com.example.gamejournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Slide;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager2;
+    private Handler slideHandler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        banners();
+
     }
+
+    private void banners() {
+
+        List<SliderVideojuegos> sliderVideojuegos = new ArrayList<>();
+        sliderVideojuegos.add(new SliderVideojuegos(R.drawable.wide1));
+        sliderVideojuegos.add(new SliderVideojuegos(R.drawable.wide3));
+        sliderVideojuegos.add(new SliderVideojuegos(R.drawable.wide4));
+        sliderVideojuegos.add(new SliderVideojuegos(R.drawable.wide5));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderVideojuegos, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_ALWAYS);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1- Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.setCurrentItem(1);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                slideHandler.removeCallbacks(sliderRunnable);
+            }
+        });
+
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        slideHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        slideHandler.postDelayed(sliderRunnable, 2000);
+    }
+
+    private void initView() {
+        viewPager2 = findViewById(R.id.viewPager);
+    }
+
 }
---
+++ GameJournal/app/src/main/java/com/example/gamejournal/SliderVideojuegos.java
@@ -0,0 +1,13 @@
+package com.example.gamejournal;
+
+public class SliderVideojuegos {
+
+    private int image;
+
+    public SliderVideojuegos(int image) { this.image = image; }
+
+    public int getImage() { return image; }
+
+    public void setImage(int image) { this.image = image; }
+}
+
